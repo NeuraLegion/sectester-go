4f37f8d8c9512799fbd47e6f91656e10c8c78be1
Artem Derevnjuk <artem.derevnjuk@neuralegion.com>
2023-03-13 15:23:14 +0400

    docs(bus): add godoc for HTTP command dispatcher (#30)
    closes #26

--- bus/dispatcher/http/request.go
+++ bus/dispatcher/http/request.go
@@ -5,10 +5,16 @@ import (
 	"net/http"
 )
 
+// Request represents a payload for a command, including URL, method, parameters, body and headers.
 type Request struct {
-	Url     string
-	Method  string
-	Params  map[string]string
-	Body    io.ReadCloser
+	// Url is the URL of the request.
+	Url string
+	// Method is the HTTP method of the request, such as GET, POST, PUT, DELETE, etc.
+	Method string
+	// Params is a map of query parameters for the request.
+	Params map[string]string
+	// Body is an io.ReadCloser that contains the body of the request.
+	Body io.ReadCloser
+	// Headers is a http.Header that contains the headers of the request.
 	Headers http.Header
 }
